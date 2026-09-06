1cbd93a9e3a8e8aefb054cd921e5944e70e7009f
Raahim-abdullah <raahimabdullah441@gmail.com>
2026-09-07 03:15:10 +0500

    added test id and enabled test for anecdote

--- anecdotes/src/components/Filter.jsx
+++ anecdotes/src/components/Filter.jsx
@@ -14,7 +14,7 @@ const Filter = () => {
 
   return (
     <div style={style}>
-      filter <input onChange={handleChange} />
+      filter <input data-testid="filter" onChange={handleChange} />
     </div>
   )
 }

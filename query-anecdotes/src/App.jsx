76eb48111d807f7007d4e1df7d5a12fa7647b736
Raahim-abdullah <raahimabdullah441@gmail.com>
2026-09-08 04:51:21 +0500

    Exercise 6.16

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,35 +1,54 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes } from './request'
 
 const App = () => {
+  const result = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: async () => {
+      return await getAnecdotes()
+    }
+  })
+
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      content: 'If it hurts, do it more often',
-      id: '47145',
-      votes: 0,
-    },
-  ]
+  const anecdotes = result.data
 
   return (
     <div>
-      <h3>Anecdote app</h3>
+      {
+        result.isPending ? (
+          <div>loading data...</div>
+        ) : (
+          <>
+            {result.isError ? (
+              <div>anecdote service is not available due to problem in server</div>
+            ) : null}
 
-      <Notification />
-      <AnecdoteForm />
+            {result.isSuccess ? (
+              <div>
+                <h3>Anecdote app</h3>
 
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
-          </div>
-        </div>
-      ))}
+                <Notification />
+                <AnecdoteForm />
+
+                {anecdotes.map((anecdote) => (
+                  <div key={anecdote.id}>
+                    <div>{anecdote.content}</div>
+                    <div>
+                      has {anecdote.votes}
+                      <button onClick={() => handleVote(anecdote)}>vote</button>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            ) : null}
+          </>
+        )
+      }
     </div>
   )
 }
